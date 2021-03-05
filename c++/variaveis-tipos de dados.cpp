9997c18058604c721783550123c6e9d317a05254
Ronald Santos <ronaldcontact2019@gmail.com>
2021-03-04 21:24:19 -0300

    Ajustes em Variáveis e tipos de dados.

---
+++ c++/variaveis-tipos de dados.cpp
@@ -0,0 +1,25 @@
+#include <iostream>;
+
+using namespace std;
+
+int main() {
+
+  // Tipos primitivos.
+
+  bool booleano;  
+  bool Booleano = false;     // Bool, 1bit apenas, representa V ou F.
+
+  int numero1 = 14;
+  int numero2 = 23;
+
+  char letra1 = '$'; // Um caracter.
+  char letra2 = '#';
+
+  float decimal1 = 12.3;   // números decimais.
+  double decimal2 = 23.3;  //consegui tratar duas vezes mais que o float.
+
+  return 0;
+
+  cout << Booleano << endl;
+
+}
