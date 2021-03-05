54268a9ffb9ef9f9ade1719472047b7acec01bf4
Ronald Santos <ronaldcontact2019@gmail.com>
2021-03-05 13:46:40 -0300

    C++ Operadores Relacionais

---
+++ c++/operacoes-matematicas-operadores relacionais.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    
+    // Operadores Relacionais
+    
+    // <
+    // >
+    // ==
+    // <=
+    // >=
+
+
+    int var1 =12;
+    int var2 =53;
+
+    bool resultado = var1 != var2;
+
+    cout << endl;
+    cout << "Resultado: " << resultado << endl;
+    cout << endl;
+
+    return 0;
+}
+
