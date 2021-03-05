c752cebf2321de66085294f3e4dfcea1f46c0825
Ronald Santos <ronaldcontact2019@gmail.com>
2021-03-05 13:55:16 -0300

    C++ Operadores logoco: **&&

---
+++ c++/operacoes-matematicas-operadores logicos.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    
+    // Operadores Relacionais
+    
+    // &&
+    // ||
+    // !
+
+
+    int var1 =12;
+    int var2 =12;
+
+    bool resultado = var1 == var2 && 12 == 12;
+
+    cout << endl;
+    cout << "Resultado &&: " << resultado << endl;
+    cout << endl;
+
+    return 0;
+}
+
