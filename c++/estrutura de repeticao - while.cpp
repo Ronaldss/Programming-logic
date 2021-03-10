488894b01b7fc2de9f539c5e52c43610a9e03467
Ronald Santos <ronaldcontact2019@gmail.com>
2021-03-10 12:28:21 -0300

    C++ Estrutura de Repeticao - While

---
+++ c++/estrutura de repeticao - while.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+
+    cout << "inicio" << endl;
+
+    int i = 0;
+
+    while(i <= 10) {
+        cout << "Valor de i: " << i << endl;
+        i++;
+    }
+    cout << "fim" << endl;
+    return 0;
+}
